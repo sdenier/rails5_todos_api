f6f93b53486906a79706cbe4c6904c79c25213b6
Simon Denier <simon@sogilis.com>
2017-09-02 18:15:12 +0200

    Add JWT authorisation service

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,7 +40,8 @@ RSpec.configure do |config|
 
   config.include FactoryGirl::Syntax::Methods
 
-  config.include RequestSpecHelper, type: :request
+  config.include ControllerSpecHelper
+  config.include RequestSpecHelper
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
