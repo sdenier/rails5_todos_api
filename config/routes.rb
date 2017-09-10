fdadfdae39bfa170b5c80c9558b3f2427cdb94cf
Simon Denier <simon@sogilis.com>
2017-09-11 00:21:10 +0200

    Add user signup & authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  post 'auth/login', to: 'authentication#authenticate'
+
+  post 'signup', to: 'users#create'
+
   resources :todos do
     resources :items
   end
