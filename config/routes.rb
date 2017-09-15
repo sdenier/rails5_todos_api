97cc8cc871c98ec80276b05c3963e9734962d612
Simon Denier <simon@sogilis.com>
2017-09-16 00:37:53 +0200

    Add dummy API v2

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   post 'signup', to: 'users#create'
 
+  scope module: :v2, constraints: ApiVersion.new('v2') do
+    resources :todos, only: :index
+  end
+
   scope module: :v1, constraints: ApiVersion.new('v1', true) do
     resources :todos do
       resources :items
